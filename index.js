5697a441f744a293c7757075d2a617ae2f55d4fa
Rasik Abdullah <rasikabdullah@gmail.com>
2023-06-05 18:13:33 +0600

    mongodb added

--- index.js
+++ index.js
@@ -1,10 +1,40 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
 const app = express();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+console.log(process.env.DB_Pass)
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.iiyr61g.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri)
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 app.get('/', (req, res) => {
     res.send('coffee server is running...')
